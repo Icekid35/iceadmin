2871602136f32f3c3d495f29dd43c531541e432d
icekid35 <bellohabib682@gmail.com>
2024-12-17 17:28:25 +0100

    first commit

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -9,12 +9,16 @@ import { cn } from "@/lib/utils"
 const Dialog = DialogPrimitive.Root
 
 const DialogTrigger = DialogPrimitive.Trigger
-
+interface DialogPortalProps extends DialogPrimitive.DialogPortalProps {
+  className?: string;
+}
 const DialogPortal = ({
   className,
   ...props
-}: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
+}: DialogPortalProps) => (
+<DialogPrimitive.Portal {...props}>
+  <div className={cn(className)}>{props.children}</div>
+</DialogPrimitive.Portal>
 )
 DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
